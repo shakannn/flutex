8aa68ab2e357c512edddb51bd06aa6e58175b1b1
IzumiShaka-desu <shakaaji29@gmail.com>
2022-10-20 13:01:20 +0700

    add new menu

--- flutex/lib/page/main_page.dart
+++ flutex/lib/page/main_page.dart
@@ -3,21 +3,32 @@ import 'package:flutex/component/button_example.dart';
 import 'package:flutex/component/column_example.dart';
 import 'package:flutex/component/row_example.dart';
 import 'package:flutex/component/stack_example.dart';
+import 'package:flutex/component/wrap_example.dart';
 import 'package:flutter/material.dart';
 
 class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    List<Widget> list=[IconExample(),RowExample(),ColumnExample(),ButtonExample(),StackExample()];
+    List<Widget> list = [
+      IconExample(),
+      RowExample(),
+      ColumnExample(),
+      ButtonExample(),
+      StackExample(),
+      WrapExample()
+    ];
     return Scaffold(
-      appBar: AppBar(title:Text('example')),
-      body: Container(child: 
-      ListView.builder(
-        itemCount: list.length,
-        itemBuilder: (ctx,index)=>GestureDetector(
-          onTap: ()=>Navigator.of(context).push(MaterialPageRoute(builder:(ctx)=>list[index])),
-          child: ListTile(title: Text(list[index].toString()),)))
-      ,),
+      appBar: AppBar(title: Text('example')),
+      body: Container(
+        child: ListView.builder(
+            itemCount: list.length,
+            itemBuilder: (ctx, index) => GestureDetector(
+                onTap: () => Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (ctx) => list[index])),
+                child: ListTile(
+                  title: Text(list[index].toString()),
+                ))),
+      ),
     );
   }
 }
